cfb74d213ab420f52ffbce875c36d30811587319
giovana-morais <giovana.vmorais@gmail.com>
2017-09-27 20:01:01 -0300

    adição da implementação de comandos com argumentos

--- projeto_2/shell.c
+++ projeto_2/shell.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -15,36 +16,52 @@
  * 3. entrada e saída padrão 
  */
 
+/* DUVIDAS
+	1. o que fazer no caso do cd? (cd não roda)
+	2. comandos que rodam em segundo plano podem receber argumentos?
+	2.1 aparentemente, os comandos já estão sendo rodados em segundo plano. o firefox, por exemplo, funciona. a calculadora não funciona.
+	3. devemos permitir uma sequência de comandos? ex: firefox & ls -al 
+*/
 int main() {
-  char comando[MAX], *token, **args;
-  int pid, i = 0;
+	char comando[MAX], *token, **args;
+	int pid, i;
 
-  args = malloc(MAX * sizeof(char *));
-  while (1) {
-    printf("> ");
-    fgets(comando, MAX, stdin);
-    strtok(comando, "\n");
-    token =  strtok(comando, " ");
-    while(token != NULL){
-        printf("token: %s\n", token);
-        args[i] = token;
-        printf("args[i]: %s\n", args[i]);
-        token =  strtok(NULL, " ");
-        i++;
-    }
+	args = malloc(MAX * sizeof(char *));
+	while (1) {
+		printf("> ");
+		__fpurge(stdin);
+		fgets(comando, MAX, stdin);
+		strtok(comando, "\n");
+		token =  strtok(comando, " ");
+		i = 0;
+		while(token != NULL){
+			args[i] = token;
+			token =  strtok(NULL, " ");
+			i++;
+		}
+		args[i++] = '\0';
 
-    
-    if (!strcmp(comando, "exit")) {
-      exit(EXIT_SUCCESS);
-    }
+		for(i = 0; args[i] != NULL; i++)
+			printf("arg[i] %s\n", args[i]);
 
-    pid = fork();
-    if (pid) {
-      waitpid(pid, NULL, 0); 
-    } else {
-      execlp(comando, comando, NULL);
-      printf("Erro ao executar comando!\n");
-      exit(EXIT_FAILURE);
-    }
-  }
+
+		if (!strcmp(comando, "exit")) {
+			exit(EXIT_SUCCESS);
+		}
+
+		pid = fork();
+		if (pid) {
+			waitpid(pid, NULL, 0); 
+		} else {
+			//execlp(comando, comando, NULL);
+			if(execvp(comando, args) < 0) {
+				for(i = 0; args[i] != NULL; i++){
+					free(args[i]);
+				}
+				free(args);
+				printf("Erro ao executar comando!\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
