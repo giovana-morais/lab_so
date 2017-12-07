3fc753a03b3f403622488ca8ee90ffb0dc7707bb
GabrielGiovana <giovana.vmorais@gmail.com>
2017-12-07 10:01:24 -0200

    começo da parte 2 do trabalho final

--- projeto_4/rsfs/fs.c
+++ projeto_4/rsfs/fs.c
@@ -43,6 +43,10 @@
 #define TAM_FAT 65536
 #define QTD_SETOR_DIR 8
 #define INICIO_DIR 256
+#define F_OPEN 0
+#define F_READ 1
+#define F_WRITE 2
+
 
 unsigned short fat[65536];
 
@@ -55,6 +59,14 @@ typedef struct {
 
 dir_entry dir[128];
 
+typedef struct {
+	unsigned short first_block; //-1 é vazio
+	int mode;//-1 vazio
+	int f_mode;	//f_mode será 0 para open, 1 para read , 2 para write e -1 pra vazio
+} id_arquivo;// Estrutura utilizada como identificador de arquivos
+
+id_arquivo id_arq[128];
+
 /* Carrega informações existentes para as estruturas em memória e checa por
  * possíveis inconsistências. Nesse caso, avisa que o disco não está
  * formatado */
@@ -62,7 +74,7 @@ int fs_init() {
  	int i = 0, format = 0, j = 0;
  	char *buffer_fat = (char *) fat;
  	char *buffer_dir = (char *) dir;
-   
+
     // carrega dados do disco
     for (i = 0; i < INICIO_DIR; i++){
 		if(!bl_read(i, &buffer_fat[i*SECTORSIZE]))
@@ -76,14 +88,19 @@ int fs_init() {
 			break;
 		}
 	}
-	
+
 	if(format)
         printf("O disco não está formatado\n");
     else {
         for (i = 0, j = INICIO_DIR; i < QTD_SETOR_DIR; i++){
             if(!bl_read(j + i, &buffer_dir[i*SECTORSIZE]))
                 return 0;
-        }	
+        }
+    }
+    for(i = 0; i < 128; i++){
+      id_arq[i].first_block = -1;
+      id_arq[i].mode = -1;
+      id_arq[i].f_mode = -1;
     }
 	return 1;
 }
@@ -93,23 +110,23 @@ int fs_format() {
  	int i = 0, j;
  	char *buffer_fat = (char *) fat;
  	char *buffer_dir = (char *) dir;
-  
+
   	// 70-77 SETA AS POSIÇÕES DA FAT, DO DIRETORIO E DOS ESPAÇOS LIVRES EM MEMÓRIA
   	for (i = 0; i < 32; i++)
   		fat[i] = A_FAT;
-  	
+
   	fat[32] = A_DIR;
-  
+
   	for (i = 33; i < 65536; i++)
   		fat[i] = LIVRE;
-  		
+
 	// INICIALIZA A STRUCT DIRETORIO
 	for(i = 0; i < 128; i++){
 	  	dir[i].used = DIR_LIVRE;
   		//dir[i].first_block = ULTIMO;
   		dir[i].size = 0;
   	}
-  	
+
   	// ESCREVE NO ARQUIVO IMAGEM A FAT E O DIRETORIO
 	for (i = 0; i < INICIO_DIR; i++)
 		if(!bl_write(i, &buffer_fat[i*SECTORSIZE]))
@@ -118,7 +135,7 @@ int fs_format() {
 	for (i = 0, j = INICIO_DIR; i < QTD_SETOR_DIR; i++)
 		if(!bl_write(j + i, &buffer_dir[i*SECTORSIZE]))
 			return 0;
-	
+
 	return 1;
 }
 
@@ -154,12 +171,12 @@ int fs_list(char *buffer, int size) {
     }
 
     strcpy(buffer, buffer_mem);
-    return 0;
+    return 1;
 }
 
 /* Cria um arquivo de tamanho 0 na primeira posição livre encontrada */
 int fs_create(char* file_name) {
-    int pos_dir = -1, pos_fat, i, j;   
+    int pos_dir = -1, pos_fat, i, j;
     char *buffer_fat = (char*) fat;
     char *buffer_dir = (char*) dir;
 
@@ -171,7 +188,7 @@ int fs_create(char* file_name) {
             break;
         }
     }
-    
+
     // busca a primeira posição livre no vetor de diretórios
     for(i = 0; i < 128; i++){
         if(dir[i].used == 0){
@@ -185,13 +202,13 @@ int fs_create(char* file_name) {
         printf("Não há espaço livre!\n");
         return 0;
     }
-    
+
     if(strlen(file_name) > 25){
         printf("Nome do arquivo precisa ter menos de 25 caracteres!\n");
         return 0;
     }
-   
-    // seta infos dir 
+
+    // seta infos dir
     dir[pos_dir].used = DIR_USADO;
     dir[pos_dir].size = 0;
     dir[pos_dir].first_block = pos_fat;
@@ -219,7 +236,7 @@ int fs_remove(char *file_name) {
     int pos_dir, pos_fat, i, j, flag = 0;
     char *buffer_fat = (char*) fat;
     char *buffer_dir = (char*) dir;
-    
+
     /* ----------- MANIPULAÇÃO EM MEMÓRIA ------------- */
     for(i = 0; i < 128; i++){
         if(!strcmp(dir[i].name, file_name)){
@@ -229,21 +246,21 @@ int fs_remove(char *file_name) {
             break;
         }
     }
-    
+
     if(!flag){
         printf("Arquivo não encontrado\n");
         return 0;
     }
-     
+
     memset(dir[pos_dir].name, '\0', strlen(dir[pos_dir].name));
     dir[pos_dir].first_block = 0;
     dir[pos_dir].used = DIR_LIVRE;
     dir[pos_dir].size = 0;
-    
+
     fat[pos_fat] = LIVRE;
 
     /* ----------- MANIPULAÇÃO EM DISCO ------------- */
-    for(i = 0; i < INICIO_DIR; i++) {  
+    for(i = 0; i < INICIO_DIR; i++) {
         if(!bl_write(i, &buffer_fat[i * SECTORSIZE]))
             return 0;
     }
@@ -261,7 +278,47 @@ int fs_remove(char *file_name) {
  *  independente dos 4K que temos que puxar do disco.
  */
 int fs_open(char *file_name, int mode) {
-  printf("Função não implementada: fs_open\n");
+	//printf("Função não implementada: fs_open\n");
+	//open prepara estruturas para serem utilizadas no read e write, gerar um identificador pro arquivo.
+	int i = 0, pos_dir, pos_fat, flag = 0;
+
+	//Nesse for iremos percorrer todo o diretório em busca de um arquivo com o nome file_name
+	for(i = 0; i < 128; i++){
+		if(!strcmp(dir[i].name, file_name)){
+		    pos_dir = i;
+		    pos_fat = dir[i].first_block;
+		    flag = 1;							//Caso esse arquivo seja encontrado, iremos sinalizar
+		    break;
+		}
+	}
+
+	if (mode == FS_R){
+		//Se entrar aqui, significa que o arquivo não existe e um erro deve ser gerado
+		if (!flag){
+			printf("Arquivo nao existe\n");
+			return -1;
+		} else {	//Caso entre aqui, significa que o arquivo existe
+      for(i = 0; i < 128; i++){
+        if(id_arq[i].first_block = -1){
+          id_arq[i].first_block = pos_fat;
+          id_arq[i].mode = mode;
+          id_arq[i].f_mode = 1;
+          return i;
+        }
+      }
+			//Implementar fopen (blread e blwrite)
+		}
+	} else if (mode == FS_W){
+		//Se entrar aqui, significa que o arquivo não existe e então iremos criá-lo.
+		if (!flag){
+			fs_create(file_name);
+		} else {
+			fs_remove(file_name);
+			fs_create(file_name);
+		}
+	}
+
+
   return -1;
 }
 
@@ -284,9 +341,9 @@ int fs_write(char *buffer, int size, int file) {
  *  > Cria um marcador de leitura pra esse buffer pq cada leitura vai
  *  retornar esses bytes e ver se existem
  *      > Ficar atento pra quando estiver perto do fim pq se passar do número
- *      de bytes do final, vai ter que recarregar o buffer com os prox 4096 
+ *      de bytes do final, vai ter que recarregar o buffer com os prox 4096
  *      bytes do disco
- *      > Checar fim do buffer do usuário, fim do arquivo e fim do buffer do 
+ *      > Checar fim do buffer do usuário, fim do arquivo e fim do buffer do
  *      usuário
  */
 int fs_read(char *buffer, int size, int file) {
